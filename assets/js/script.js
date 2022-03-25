a857456bacaaca9b8a016b99ce7d65de6ba120ce
Ethan Cahill <andersmith29@gmail.comhtml lang=en  head    meta charset=UTF-8 /  /head  body    p      I LOVE PANDASgit config --global user.email andersmith29@gmail.com>
2022-03-25 18:17:58 -0400

    made it cooler

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,7 @@ var MainBody = document.getElementById("big-almond")
 var TheHeader= document.getElementById("theheader")
 var MegaHeader= document.getElementById("megaheader")
 var locationtag= document.getElementById("location-tag")
+// var test = AlmondTime()
 
 function AlmondTime(){
     var name = prompt("Please Enter Your First Name");
@@ -12,6 +13,12 @@ function AlmondTime(){
         TheHeader.style.color ="red";
         TheHeader.textContent = name + " is now a proud supporter of Almonds and Hates the Environment";
         locationtag.textContent = ""
+        BigAlmond.removeEventListener("click", AlmondTime)
+        BigAlmond.addEventListener('click', function(){
+            alert('Hey ' + name + ' that tickles!')
+        })
+        
+        return name
     }
     else if (name == 'bubba'||name == 'Bubba'|| name == 'will'|| name== 'Will'||name =="William"|| name =="william"){
         alert(name + ", you are now under the power of big almond. And Alabama can suck a butt!");
@@ -23,6 +30,12 @@ function AlmondTime(){
         locationtag.style.background= "black"
         locationtag.textContent = 'Charge On'
         TheHeader.textContent = name + " is now a proud supporter of Almonds and Hates the Environment. Alabama Sucks, Go Knights and Charge On!";
+        BigAlmond.removeEventListener("click", AlmondTime)
+        BigAlmond.addEventListener('click', function(){
+            alert('Hey ' + name + ' Charge On')
+        })
+        return name
+        
     }
     else {
         alert('Hey idiot! ' + name + ' is not your name, and Big Almond Knows it!')
@@ -31,4 +44,23 @@ function AlmondTime(){
 }
 
 
+
+// function ToggleAlmond() {
+//     if (BigAlmond.src=="./assets/images/big_almond.jpeg"){
+//         BigAlmond.src='./assets/images/ashlyn.jpeg'
+    
+//     } else {
+//         BigAlmond.src="./assets/images/big_almond.jpeg"
+//     }
+// }
+
+// function AshlynTime(){
+//     if (test== 'Ethan' || test == 'Agustina' || test== 'Ashlyn' || test== 'Amy' || test== 'Kyle' ||test== 'Kade' || test =='Tiffany'||test == 'ethan' || test == 'agustina' ||test == 'ashlyn' || test == 'amy' || test== 'kyle' ||test== 'kade' || test=='tiffany'||test=='stu'||test=='stuart'||test=='Stuart'||test=='Stu')
+//     ToggleAlmond()
+// }
+
+
 BigAlmond.addEventListener("click", AlmondTime)
+// BigAlmond.addEventListener("mouseover", AshlynTime)
+
+
